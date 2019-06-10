14d6405ee2d22a136b22f20bd8561edcc14a9a0c
unknown <jorgeeliecer88@gmail.com>
2019-06-09 23:56:47 -0500

    make it better

--- src/servidor.js
+++ src/servidor.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const port = process.env.PORT || 3000;
 
 const PORT = 3000;
 var app = express();
@@ -8,8 +9,8 @@ const directorio_publico = path.join(__dirname, "../public");
 
 app.use(express.static(directorio_publico));
 
-app.listen(PORT, () => {
-  console.log(">> inicio servidor express port:" + PORT);
+app.listen(port, () => {
+  console.log(">> inicio servidor express port:" + port);
 });
 
 module.exports = { app, path };
